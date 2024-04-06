b31207f500242dd17a0b801e3e2f571cf9564ded
Absjune <156971408+Absjune@users.noreply.github.com>
2024-04-05 21:12:15 -0400

    Create producer.cpp

---
+++ Project1/producer.cpp
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <semaphore.h>
+#include <unistd.h>
+#include <iostream>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+
+int main(){
+    int Shared;
+    int* table;
+
+    //Allocate the shared memory
+    Shared = shm_open("table",create | O_RDWR, 0666); //Create the table
+    ftruncate(Shared, sizeof(int)); //Setting the size of the shared memory
+    table = static_cast<int*>(mmap(0,sizeof(int),protRead | protWrite, mapShareMem, Shared, 0)); //Mapping object to address
+    sem_t* full = sem_open("full",create,0666,0);
+    sem_t* empty = sem_open("empty",create,0666,3);
+    sem_t* mutex = sem_open("mutex",create, 0666, 1);
+
+    std::cout << "Process is running!" << std::endl;
+
+    for (int i = 0; i < 5; ++i) {
+        semaphoreWait(empty);
+        sleep(1); // sleep for 1 sec
+
+        semaphoreWait(mutex); // unlock
+        if (*table < 2) { // if table isn't full: (that is, the table does not have 2 items))
+            ++(*table);    // add new item in the table
+            std::cout << "Produced an item." << std::endl << "Table contains: " << *table << " items";
+        }
+        else {
+            std::cout << "Table is full!";
+        }
+        semaphorePost(mutex); // close the mutex
+        semaphorePost(full);
+    }
+    sleep(3);   // sleep for 1 sec
+    std::cout << "Press enter to exit the completed  process.";
+
+    //Closing semaphores
+
+    semaphoreClose(full);
+    semaphoreClose(empty);
+    semaphoreClose(mutex);
+
+    //Unlink semaphores
+
+    semaphoreUnlink("full");
+    semaphoreUnlink("empty");
+    semaphoreUnlink("mutex");
+
+    //Deallocate the shared memory
+    munmap(table, sizeof(int));
+    close(Shared);
+    shm_unlink("table");
+    return 0;
+}
